8ed92ec6537d3325cdd39ab16d78421f9e3afd22
Bryonpectol <46723917+Bryonpectol@users.noreply.github.com>
2023-05-30 08:30:09 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
+const events = [{"year":"4000 B.C.","events":"- The Sumerian civilization emerges in Mesopotamia (modern-day Iraq)."},
+                {"year":"3100 B.C.","events":"- The first Egyptian dynasty is established by Narmer (Menes), uniting Upper and Lower Egypt."},
+                {"year":"2500 B.C.","events":"- The Great Pyramid of Giza is built in Egypt."}];
+
+
 
