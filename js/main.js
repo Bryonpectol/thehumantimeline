ed6a6662ef2ef7697313ef426d9189c2b012655d
Bryonpectol <46723917+Bryonpectol@users.noreply.github.com>
2024-03-10 15:19:19 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-const events = [{"year":"4000 B.C.","events":"- The Sumerian civilization emerges in Mesopotamia (modern-day Iraq)."},
+const events = [{"year":"2500 B.C.","events":"- The Great Pyramid of Giza is built in Egypt."},
                 {"year":"3100 B.C.","events":"- The first Egyptian dynasty is established by Narmer (Menes), uniting Upper and Lower Egypt."},
-                {"year":"2500 B.C.","events":"- The Great Pyramid of Giza is built in Egypt."}];
+                {"year":"4000 B.C.","events":"- The Sumerian civilization emerges in Mesopotamia (modern-day Iraq)."},];
 
 
 let container = document.getElementById("timelinemain");
