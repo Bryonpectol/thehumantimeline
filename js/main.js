acedcfcd4500ff76c817b63a06da6c8e873064e2
Bryonpectol <46723917+Bryonpectol@users.noreply.github.com>
2024-03-10 23:06:49 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
 const events = [
-  {"year":"49-51","image":"","events":"The Apostle Paul undertakes his missionary journeys, spreading Christianity throughout the eastern Mediterranean."},
-  {"year":"43","image":"","events":"The Roman conquest of Britain begins under Emperor Claudius."},
-  {"year":"37","image":"","events":"Tiberius dies, and Caligula becomes the third Roman emperor."},
-  {"year":"30","image":"","events":"According to Christian tradition, Jesus is crucified and resurrected."},
-  {"year":"14","image":"","events":"Augustus, the first Roman emperor, dies, and Tiberius succeeds him."},
-  {"year":"9","image":"","events":"Germanic tribes, led by Arminius, defeat three Roman legions in the Battle of the Teutoburg Forest."},
-  {"year":"1","image":"/images/1.jpg","events":"The year traditionally believed to be the birth of Jesus Christ."},
-  {"year":"27 B.C.","image":"","events":"The Roman Senate grants Octavian (Augustus) the title of Augustus, marking the beginning of the Roman Empire."},
-  {"year":"44 B.C.","image":"","events":"Julius Caesar is assassinated, leading to the rise of Augustus Caesar and the Roman Empire."},
-  {"year":"146 B.C.","image":"","events":"The Roman Republic destroys Carthage, ending the Third Punic War."},
-  {"year":"221 B.C.","image":"","events":"Qin Shi Huang becomes the first emperor of unified China, marking the start of the Qin Dynasty."},
-  {"year":"323 B.C.","image":"","events":"Alexander the Great dies, and his empire is divided among his generals, leading to the Hellenistic period."},
-  {"year":"336 B.C.","image":"","events":"Alexander the Great becomes the ruler of Macedon."},
-  {"year":"490 B.C.","image":"/images/490bc.jpg","events":"The Battle of Marathon takes place, where the Athenians defeat the Persian invasion."},
-  {"year":"509 B.C.","image":"","events":"The Roman Republic is established after the overthrow of the Roman monarchy."},
-  {"year":"546 B.C.","image":"","events":"Cyrus the Great establishes the Persian Empire after overthrowing the Median Empire."},
-  {"year":"586 B.C.","image":"","events":"The Babylonians, led by Nebuchadnezzar II, conquer Jerusalem and exile the Israelites."},
-  {"year":"753 B.C.","image":"","events":"According to Roman mythology, Romulus founds the city of Rome, marking the beginning of the Roman Empire."},
-  {"year":"1200 B.C.","image":"","events":"The collapse of the Bronze Age civilizations, including the Hittites, Mycenaeans, and the fall of Troy."},
-  {"year":"1792 B.C.","image":"","events":"Hammurabi becomes the ruler of Babylon and later establishes the Code of Hammurabi, one of the earliest known legal codes."},
-  {"year":"2500 B.C.","image":"","events":"The Great Pyramid of Giza is built in Egypt."},
-  {"year":"3100 B.C.","image":"","events":"The first Egyptian dynasty is established by Narmer (Menes), uniting Upper and Lower Egypt."},
-  {"year":"4000 B.C.","image":"","events":"The Sumerian civilization emerges in Mesopotamia (modern-day Iraq)."},
-  {"year":"10,000 B.C.","image":"/images/10000bc.jpg","events":"Humanity was still in the prehistoric era, known as the Stone Age."},
+  {"year":"49-51","time":"A.D.","image":"","events":"The Apostle Paul undertakes his missionary journeys, spreading Christianity throughout the eastern Mediterranean."},
+  {"year":"43","time":"A.D.","image":"","events":"The Roman conquest of Britain begins under Emperor Claudius."},
+  {"year":"37","time":"A.D.","image":"","events":"Tiberius dies, and Caligula becomes the third Roman emperor."},
+  {"year":"30","time":"A.D.","image":"","events":"According to Christian tradition, Jesus is crucified and resurrected."},
+  {"year":"14","time":"A.D.","image":"","events":"Augustus, the first Roman emperor, dies, and Tiberius succeeds him."},
+  {"year":"9","time":"A.D.","image":"","events":"Germanic tribes, led by Arminius, defeat three Roman legions in the Battle of the Teutoburg Forest."},
+  {"year":"1","time":"A.D.","image":"/images/1.jpg","events":"The year traditionally believed to be the birth of Jesus Christ."},
+  {"year":"27","time":"B.C.","image":"","events":"The Roman Senate grants Octavian (Augustus) the title of Augustus, marking the beginning of the Roman Empire."},
+  {"year":"44","time":"B.C.","image":"","events":"Julius Caesar is assassinated, leading to the rise of Augustus Caesar and the Roman Empire."},
+  {"year":"146","time":"B.C.","image":"","events":"The Roman Republic destroys Carthage, ending the Third Punic War."},
+  {"year":"221","time":"B.C.","image":"","events":"Qin Shi Huang becomes the first emperor of unified China, marking the start of the Qin Dynasty."},
+  {"year":"323","time":"B.C.","image":"","events":"Alexander the Great dies, and his empire is divided among his generals, leading to the Hellenistic period."},
+  {"year":"336","time":"B.C.","image":"","events":"Alexander the Great becomes the ruler of Macedon."},
+  {"year":"490","time":"B.C.","image":"/images/490bc.jpg","events":"The Battle of Marathon takes place, where the Athenians defeat the Persian invasion."},
+  {"year":"509","time":"B.C.","image":"","events":"The Roman Republic is established after the overthrow of the Roman monarchy."},
+  {"year":"546","time":"B.C.","image":"","events":"Cyrus the Great establishes the Persian Empire after overthrowing the Median Empire."},
+  {"year":"586","time":"B.C.","image":"","events":"The Babylonians, led by Nebuchadnezzar II, conquer Jerusalem and exile the Israelites."},
+  {"year":"753","time":"B.C.","image":"","events":"According to Roman mythology, Romulus founds the city of Rome, marking the beginning of the Roman Empire."},
+  {"year":"1200","time":"B.C.","image":"","events":"The collapse of the Bronze Age civilizations, including the Hittites, Mycenaeans, and the fall of Troy."},
+  {"year":"1792","time":"B.C.","image":"","events":"Hammurabi becomes the ruler of Babylon and later establishes the Code of Hammurabi, one of the earliest known legal codes."},
+  {"year":"2500","time":"B.C.","image":"","events":"The Great Pyramid of Giza is built in Egypt."},
+  {"year":"3100","time":"B.C.","image":"","events":"The first Egyptian dynasty is established by Narmer (Menes), uniting Upper and Lower Egypt."},
+  {"year":"4000","time":"B.C.","image":"","events":"The Sumerian civilization emerges in Mesopotamia (modern-day Iraq)."},
+  {"year":"10,000","time":"B.C.","image":"/images/10000bc.jpg","events":"Humanity was still in the prehistoric era, known as the Stone Age."},
   ];
 
 
@@ -57,7 +57,7 @@ events.forEach((Events) => {
     Year.className = "yeartext";
     Image.className = "image";
 
-    Year.textContent = "Year: " + Events.year;
+    Year.textContent = "Year: " + Events.year + " " + Events.time;
     Event.textContent = "" + Events.events;
     Image.setAttribute("src", Events.image);
 
