c6f812f49f33d9fdecf942c430bd8b89e2483284
Bryonpectol <46723917+Bryonpectol@users.noreply.github.com>
2024-03-10 22:14:52 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -61,11 +61,11 @@ function submit() {
   searchYear.textContent = "Searched for: " + searchInput.value;
 
   let filteredItems = events.filter((item) => item.year.includes(searchInput.value));
+  let jsoned = JSON.stringify(filteredItems);
     //let filteredItems = events.filter((item) => item.year = (searchInput.value));
-  searchYearReal.textContent = "" + filteredItems;
-  let jsoned = JSON.stringify(searchYearReal);
+  searchYearReal.textContent = "" + jsoned;
 
   Form.appendChild(searchYear);
-  Form.appendChild(jsoned);
+  Form.appendChild(searchYearReal);
 };
 
